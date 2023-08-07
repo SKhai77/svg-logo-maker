621396d1a2889e6392f49cef9b077468770ccc99
SKhai77 <orionkhai@gmail.com>
2023-08-07 09:15:17 -0500

    updated function name, and fixed layout of text question

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const SVG = require("./lib/svg");
 const { Circle, Triangle, Square } = require("./lib/shapes");
 const { writeFile } = require("fs/promises")
 
-const application = () => {
+const generateSVG = () => {
     inquirer.prompt([
         {
             type: "list",
@@ -19,12 +19,16 @@ const application = () => {
         {
             type: "input",
             name: "text",
-            message: "What would you like the text to say, in your svg?\n1-3 characters"
+            message: "What would you like the text to say, in your svg? (1-3 characters)"
         },
         {
             type: "input",
             name: "textColor",
             message: "What color would you like your text to be?"
         }
-    ])
+    ]).then((res) => {
+        console.log(res)
+    })
 }
+
+generateSVG()
