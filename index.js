11c393bb2f9741434ae96ad81a4fe90d4f5dee75
SKhai77 <orionkhai@gmail.com>
2023-08-07 09:13:07 -0500

    set up intitial inquirer.prompt questions

---
+++ index.js
@@ -0,0 +1,30 @@
+const inquirer = require("inquirer");
+const SVG = require("./lib/svg");
+const { Circle, Triangle, Square } = require("./lib/shapes");
+const { writeFile } = require("fs/promises")
+
+const application = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "shape",
+            message: "Which shape would you like your svg to me?",
+            choices: ["Circle", "Triangle", "Square"]
+        },
+        {
+            type: "input",
+            name: "shapeColor",
+            message: "What color would you like your svg to be?"
+        },
+        {
+            type: "input",
+            name: "text",
+            message: "What would you like the text to say, in your svg?\n1-3 characters"
+        },
+        {
+            type: "input",
+            name: "textColor",
+            message: "What color would you like your text to be?"
+        }
+    ])
+}
