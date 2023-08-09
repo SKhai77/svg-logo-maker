b8ca961be74f69df7edc9f2de34cba9bd2b2a4c5
SKhai77 <orionkhai@gmail.com>
2023-08-09 12:32:07 -0500

    Refined and added comment to the codes

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+// Imported packages and modules needed for this application
 const inquirer = require("inquirer");
 const SVG = require("./lib/svg");
 const { Circle, Triangle, Square } = require("./lib/shapes");
-const { writeFile } = require("fs/promises")
+const { writeFile } = require("fs/promises");
 
+// An array of questions for user input
 const generateSVG = () => {
     inquirer.prompt([
         {
             type: "list",
             name: "shape",
-            message: "Which shape would you like your svg to me?",
+            message: "Which shape would you like your svg to be?",
             choices: ["Circle", "Triangle", "Square"]
         },
         {
@@ -26,27 +28,38 @@ const generateSVG = () => {
             name: "textColor",
             message: "What color would you like your text to be?"
         }
-    ]).then((res) => {
-        console.log(res)
+    ])
+    // Output the user's response object for debugging purposes
+    .then((res) => {
+        console.log(res);
+
+        // Declare a variable to store the SVG shape object
         let svgShape;
-        if(res.shape === "Circle") {
-            svgShape = new Circle()
+
+        // Create a new Circle shape object if the user input is "Circle"
+        if (res.shape === "Circle") {
+            svgShape = new Circle();
         }
-        if(res.shape === "Triangle") {
-            svgShape = new Triangle()
+        // Create a new Triangle shape object if the user input is "Triangle"
+        if (res.shape === "Triangle") {
+            svgShape = new Triangle();
         }
-        if(res.shape === "Square") {
-            svgShape = new Square()
+        // Create a new Square shape object if the user input is "Square"
+        if (res.shape === "Square") {
+            svgShape = new Square();
         }
+        // Set the color of the shape object based on the user input
         svgShape.setColor(res.shapeColor)
 
-        const svg = new SVG()
-        svg.setText(res.text, res.textColor)
-        svg.setShape(svgShape)
-        return writeFile("logo.svg", svg.render())
+        // Create a function to generates svg file and to save it in the examples folder
+        const svg = new SVG();
+        svg.setText(res.text, res.textColor);
+        svg.setShape(svgShape);
+        return writeFile("./examples/logo.svg", svg.render());
     }).then(() => {
-        console.log("Generated logo.svg")
+        console.log("Your SVG logo has been successfully generated as a logo.svg in examples folder");
     })
-}
+};
 
-generateSVG()
+// Function call to generates SVG file
+generateSVG();
