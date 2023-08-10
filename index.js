7c4a12edb81fc632a3b01b999ab696f1d443f557
SKhai77 <orionkhai@gmail.com>
2023-08-10 00:09:16 -0500

    Formated the document for better readability

--- index.js
+++ index.js
@@ -29,36 +29,36 @@ const generateSVG = () => {
             message: "What color would you like your text to be?"
         }
     ])
-    // Output the user's response object for debugging purposes
-    .then((res) => {
-        console.log(res);
+        // Output the user's response object for debugging purposes
+        .then((res) => {
+            console.log(res);
 
-        // Declare a variable to store the SVG shape object
-        let svgShape;
+            // Declare a variable to store the SVG shape object
+            let svgShape;
 
-        // Create a new Circle shape object if the user input is "Circle"
-        if (res.shape === "Circle") {
-            svgShape = new Circle();
-        }
-        // Create a new Triangle shape object if the user input is "Triangle"
-        if (res.shape === "Triangle") {
-            svgShape = new Triangle();
-        }
-        // Create a new Square shape object if the user input is "Square"
-        if (res.shape === "Square") {
-            svgShape = new Square();
-        }
-        // Set the color of the shape object based on the user input
-        svgShape.setColor(res.shapeColor)
+            // Create a new Circle shape object if the user input is "Circle"
+            if (res.shape === "Circle") {
+                svgShape = new Circle();
+            }
+            // Create a new Triangle shape object if the user input is "Triangle"
+            if (res.shape === "Triangle") {
+                svgShape = new Triangle();
+            }
+            // Create a new Square shape object if the user input is "Square"
+            if (res.shape === "Square") {
+                svgShape = new Square();
+            }
+            // Set the color of the shape object based on the user input
+            svgShape.setColor(res.shapeColor)
 
-        // Create a function to generates svg file and to save it in the examples folder
-        const svg = new SVG();
-        svg.setText(res.text, res.textColor);
-        svg.setShape(svgShape);
-        return writeFile("./examples/logo.svg", svg.render());
-    }).then(() => {
-        console.log("Your SVG logo has been successfully generated as a logo.svg in examples folder");
-    })
+            // Create a function to generates svg file and to save it in the examples folder
+            const svg = new SVG();
+            svg.setText(res.text, res.textColor);
+            svg.setShape(svgShape);
+            return writeFile("./examples/logo.svg", svg.render());
+        }).then(() => {
+            console.log("Your SVG logo has been successfully generated as a logo.svg in examples folder");
+        })
 };
 
 // Function call to generates SVG file
